32b4200b5abc85129dd13c3a5c34d267b621e17f
j v <vantjac@gmail.com>
2022-05-23 12:47:21 -0700

    improve thumbnail rendering

--- src/ThumbnailWindow.cpp
+++ src/ThumbnailWindow.cpp
@@ -32,10 +32,19 @@ void ThumbnailWindow::onPaint(PAINTSTRUCT paint) {
     RECT bodyRect = windowBody();
     SIZE bodySize = {bodyRect.right - bodyRect.left, bodyRect.bottom - bodyRect.top};
     HBITMAP hBitmap;
+    bool hasAlpha = true;
     // TODO this should be done asynchronously!!
-    // and also the resulting bitmap should be cached
-    if (FAILED(imageFactory->GetImage(bodySize, SIIGBF_BIGGERSIZEOK, &hBitmap)))
-        return;
+    // and also the resulting bitmap should maybe be cached
+    if (FAILED(imageFactory->GetImage(bodySize,
+            SIIGBF_BIGGERSIZEOK | SIIGBF_THUMBNAILONLY, &hBitmap))) {
+        // no thumbnail, fallback to icon
+        hasAlpha = false;
+        int minDim = min(bodySize.cx, bodySize.cy);
+        // SIIGBF_ICONBACKGROUND is much faster, but only works with a square rect
+        if (FAILED(imageFactory->GetImage({minDim, minDim},
+                SIIGBF_BIGGERSIZEOK | SIIGBF_ICONONLY | SIIGBF_ICONBACKGROUND, &hBitmap)))
+            return;
+    }
     BITMAP bitmap;
     GetObject(hBitmap, sizeof(bitmap), &bitmap);
     debugPrintf(L"Scale thumbnail from %dx%d to %dx%d\n",
@@ -55,11 +64,29 @@ void ThumbnailWindow::onPaint(PAINTSTRUCT paint) {
         hDest = bodySize.cy;
     }
 
+    if (hasAlpha) {
+        // use alpha channel to composite onto a white background
+        uint8_t *bitmapBytes = (uint8_t*)bitmap.bmBits;
+        int rowIndex = 0;
+        for (int y = 0; y < bitmap.bmHeight; y++, rowIndex += bitmap.bmWidthBytes) {
+            int i = rowIndex;
+            for (int x = 0; x < bitmap.bmWidth; x++, i += 4) {
+                int alpha = bitmapBytes[i + 3];
+                if (alpha == 255)
+                    continue;
+                int c = 255 - alpha;
+                bitmapBytes[i + 0] = (uint8_t)((int)(bitmapBytes[i + 0]) * alpha / 255 + c);
+                bitmapBytes[i + 1] = (uint8_t)((int)(bitmapBytes[i + 1]) * alpha / 255 + c);
+                bitmapBytes[i + 2] = (uint8_t)((int)(bitmapBytes[i + 2]) * alpha / 255 + c);
+            }
+        }
+    }
+
     HDC hdcMem = CreateCompatibleDC(paint.hdc);
     HGDIOBJ oldBitmap = SelectObject(hdcMem, hBitmap);
-    AlphaBlend(paint.hdc, xDest, yDest, wDest, hDest,
-        hdcMem, 0, 0, bitmap.bmWidth, bitmap.bmHeight,
-        {AC_SRC_OVER, 0, 255, AC_SRC_ALPHA});
+    SetStretchBltMode(paint.hdc, HALFTONE);
+    StretchBlt(paint.hdc, xDest, yDest, wDest, hDest,
+        hdcMem, 0, 0, bitmap.bmWidth, bitmap.bmHeight, SRCCOPY);
     SelectObject(hdcMem, oldBitmap);
     DeleteDC(hdcMem);
     DeleteObject(hBitmap);
