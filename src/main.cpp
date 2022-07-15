f2064b074499454c0dce66fba6db5173e986ca6b
j v <vantjac@gmail.com>
2022-07-14 20:21:00 -0700

    Remove jump list; close #105

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,6 @@
 #include "resource.h"
 #include <shellapi.h>
 #include <shlobj.h>
-#include <propkey.h>
-#include <Propvarutil.h>
-#include <strsafe.h>
-#include <VersionHelpers.h>
 
 #pragma comment(lib, "Dwmapi.lib")
 #pragma comment(lib, "Gdi32.lib")
@@ -121,7 +117,6 @@ int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int showCommand) {
 
     WaitForSingleObject(jumpListThread, INFINITE);
     CloseHandle(jumpListThread);
-    updateJumpList(nullptr);
 
     ItemWindow::uninit();
     PreviewWindow::uninit();
@@ -139,78 +134,7 @@ DWORD WINAPI updateJumpList(void *) {
     UINT minSlots;
     CComPtr<IObjectArray> removedDestinations;
     checkHR(jumpList->BeginList(&minSlots, IID_PPV_ARGS(&removedDestinations)));
-
-    CComPtr<IObjectCollection> tasks;
-    if (!checkHR(tasks.CoCreateInstance(__uuidof(EnumerableObjectCollection))))
-        return false;
-
-    wchar_t exePath[MAX_PATH];
-    GetModuleFileName(GetModuleHandle(nullptr), exePath, MAX_PATH);
-
-    CComPtr<IShellItem> favoritesFolder;
-    if (IsWindows10OrGreater()) {
-        // Quick Access
-        checkHR(SHCreateItemFromParsingName(L"shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}",
-            nullptr, IID_PPV_ARGS(&favoritesFolder)));
-    } else {
-        checkHR(SHGetKnownFolderItem(FOLDERID_Links, KF_FLAG_DEFAULT, nullptr,
-            IID_PPV_ARGS(&favoritesFolder)));
-    }
-    if (!favoritesFolder)
-        return false;
-    CComPtr<IEnumShellItems> enumItems;
-    if (!checkHR(favoritesFolder->BindToHandler(nullptr, BHID_EnumItems, IID_PPV_ARGS(&enumItems))))
-        return false;
-
-    if (IsWindows10OrGreater()) {
-        // TODO: hack!! the first 20 items in Quick Access are recents
-        for (int i = 0; i < 20; i++) {
-            CComPtr<IShellItem> tempItem;
-            checkHR(enumItems->Next(1, &tempItem, nullptr));
-        }
-    }
-    CComPtr<IShellItem> childItem;
-    while (enumItems->Next(1, &childItem, nullptr) == S_OK) {
-        CComHeapPtr<wchar_t> displayName, parsingName;
-        if (!checkHR(childItem->GetDisplayName(SIGDN_NORMALDISPLAY, &displayName))
-                || !checkHR(childItem->GetDisplayName(SIGDN_DESKTOPABSOLUTEPARSING, &parsingName)))
-            continue;
-        wchar_t args[MAX_PATH];
-        args[0] = L'"';
-        checkHR(StringCchCopy(args + 1, MAX_PATH - 2, parsingName));
-        checkHR(StringCchCat(args, MAX_PATH, L"\""));
-
-        CComPtr<IShellLink> link;
-        if (!checkHR(link.CoCreateInstance(__uuidof(ShellLink))))
-            continue;
-        checkHR(link->SetPath(exePath));
-        checkHR(link->SetArguments(args));
-        checkHR(link->SetIconLocation(exePath, IDR_APP_ICON));
-        CComQIPtr<IPropertyStore> linkProps(link);
-        PROPVARIANT propVar;
-        if (checkHR(InitPropVariantFromString(displayName, &propVar)))
-            linkProps->SetValue(PKEY_Title, propVar);
-        CComPtr<IExtractIcon> extractIcon;
-        if (checkHR(childItem->BindToHandler(nullptr, BHID_SFUIObject,
-                IID_PPV_ARGS(&extractIcon)))) {
-            wchar_t iconFile[MAX_PATH];
-            int index;
-            UINT flags;
-            if (extractIcon->GetIconLocation(0, iconFile, MAX_PATH, &index, &flags) == S_OK) {
-                if (!(flags & GIL_NOTFILENAME))
-                    checkHR(link->SetIconLocation(iconFile, index));
-            }
-        }
-
-        checkHR(tasks->AddObject(link));
-        childItem = nullptr; // CComPtr isn't very smart
-    }
-
-    if (!checkHR(jumpList->AddUserTasks(tasks)))
-        return false;
-    if (!checkHR(jumpList->CommitList()))
-        return false;
-
-    debugPrintf(L"Successfully updated jump list\n");
-    return true;
+    // previous versions had a jump list but for now it is removed (#105)
+    // so this only serves to clear any existing jump lists
+    return checkHR(jumpList->CommitList());
 }
