21d6706793c492cd472c47ffaf02abadc79573f2
j v <vantjac@gmail.com>
2023-08-13 19:04:29 -0700

    No conflicting control IDs

--- src/resource.h
+++ src/resource.h
@@ -74,39 +74,39 @@
 #define IDC_EXPLORER_SETTINGS       1010
 
 #define IDD_SETTINGS_TRAY           105
-#define IDC_TRAY_ENABLED            1001
-#define IDC_OPEN_TRAY_ON_STARTUP    1002
-#define IDC_TRAY_FOLDER_PATH        1003
-#define IDC_TRAY_FOLDER_BROWSE      1004
-#define IDC_TRAY_DIR_ABOVE          1005
-#define IDC_TRAY_DIR_BELOW          1006
-#define IDC_TRAY_DIR_RIGHT          1007
-#define IDC_RESET_TRAY_POSITION     1008
+#define IDC_TRAY_ENABLED            1101
+#define IDC_OPEN_TRAY_ON_STARTUP    1102
+#define IDC_TRAY_FOLDER_PATH        1103
+#define IDC_TRAY_FOLDER_BROWSE      1104
+#define IDC_TRAY_DIR_ABOVE          1105
+#define IDC_TRAY_DIR_BELOW          1106
+#define IDC_TRAY_DIR_RIGHT          1107
+#define IDC_RESET_TRAY_POSITION     1108
 
 #define IDD_SETTINGS_TEXT           109
-#define IDC_TEXT_EDITOR_ENABLED     1001
-#define IDC_TEXT_FONT               1002
-#define IDC_TEXT_FONT_NAME          1003
-#define IDC_TEXT_WRAP               1004
-#define IDC_TEXT_AUTO_INDENT        1005
-#define IDC_SCRATCH_FOLDER_PATH     1006
-#define IDC_SCRATCH_FOLDER_BROWSE   1007
-#define IDC_SCRATCH_FILE_NAME       1008
-#define IDC_TEXT_NEWLINES           1009
-#define IDC_TEXT_AUTO_NEWLINES      1010
-#define IDC_TEXT_ENCODING           1011
-#define IDC_TEXT_AUTO_ENCODING      1012
+#define IDC_TEXT_EDITOR_ENABLED     1201
+#define IDC_TEXT_FONT               1202
+#define IDC_TEXT_FONT_NAME          1203
+#define IDC_TEXT_WRAP               1204
+#define IDC_TEXT_AUTO_INDENT        1205
+#define IDC_SCRATCH_FOLDER_PATH     1206
+#define IDC_SCRATCH_FOLDER_BROWSE   1207
+#define IDC_SCRATCH_FILE_NAME       1208
+#define IDC_TEXT_NEWLINES           1209
+#define IDC_TEXT_AUTO_NEWLINES      1210
+#define IDC_TEXT_ENCODING           1211
+#define IDC_TEXT_AUTO_ENCODING      1212
 
 #define IDD_SETTINGS_BROWSER        110
-#define IDC_SET_DEFAULT_BROWSER     1001
-#define IDC_RESET_DEFAULT_BROWSER   1002
+#define IDC_SET_DEFAULT_BROWSER     1301
+#define IDC_RESET_DEFAULT_BROWSER   1302
 
 #define IDD_SETTINGS_ABOUT          106
-#define IDC_UPDATES_LINK            1001
-#define IDC_HELP_LINK               1002
-#define IDC_SOURCE_LINK             1003
-#define IDC_AUTO_UPDATE             1004
-#define IDC_LEGAL_INFO              1005
+#define IDC_UPDATES_LINK            1401
+#define IDC_HELP_LINK               1402
+#define IDC_SOURCE_LINK             1403
+#define IDC_AUTO_UPDATE             1404
+#define IDC_LEGAL_INFO              1405
 
 #define IDS_SETTINGS_CAPTION    200
 #define IDS_MENU_COMMAND        201
