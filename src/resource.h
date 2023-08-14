6a376f2e36481f570148352836d9974c2b4c5dbd
j v <vantjac@gmail.com>
2023-08-13 20:39:33 -0700

    Move dialogs to separate resource file

--- src/resource.h
+++ src/resource.h
@@ -1,3 +1,6 @@
+#pragma once
+#include "dialog.h"
+
 #define CHROMAFILER_VERSION        0,6,0,0
 #define CHROMAFILER_VERSION_STRING "0.6.0\0"
 
@@ -60,54 +63,6 @@
 #define MDL2_VIEW                   L"\uE890"
 #define MDL2_SAVE                   L"\uE74E"
 #define MDL2_DELETE                 L"\uE74D"
-
-#define IDD_SETTINGS_GENERAL        104
-#define IDC_START_FOLDER_PATH       1001
-#define IDC_START_FOLDER_BROWSE     1002
-#define IDC_FOLDER_WINDOW_WIDTH     1003
-#define IDC_FOLDER_WINDOW_HEIGHT    1004
-#define IDC_ITEM_WINDOW_WIDTH       1005
-#define IDC_ITEM_WINDOW_HEIGHT      1006
-#define IDC_STATUS_TEXT_ENABLED     1007
-#define IDC_TOOLBAR_ENABLED         1008
-#define IDC_PREVIEWS_ENABLED        1009
-#define IDC_EXPLORER_SETTINGS       1010
-
-#define IDD_SETTINGS_TRAY           105
-#define IDC_TRAY_ENABLED            1101
-#define IDC_OPEN_TRAY_ON_STARTUP    1102
-#define IDC_TRAY_FOLDER_PATH        1103
-#define IDC_TRAY_FOLDER_BROWSE      1104
-#define IDC_TRAY_DIR_ABOVE          1105
-#define IDC_TRAY_DIR_BELOW          1106
-#define IDC_TRAY_DIR_RIGHT          1107
-#define IDC_RESET_TRAY_POSITION     1108
-
-#define IDD_SETTINGS_TEXT           109
-#define IDC_TEXT_EDITOR_ENABLED     1201
-#define IDC_TEXT_FONT               1202
-#define IDC_TEXT_FONT_NAME          1203
-#define IDC_TEXT_WRAP               1204
-#define IDC_TEXT_AUTO_INDENT        1205
-#define IDC_SCRATCH_FOLDER_PATH     1206
-#define IDC_SCRATCH_FOLDER_BROWSE   1207
-#define IDC_SCRATCH_FILE_NAME       1208
-#define IDC_TEXT_NEWLINES           1209
-#define IDC_TEXT_AUTO_NEWLINES      1210
-#define IDC_TEXT_ENCODING           1211
-#define IDC_TEXT_AUTO_ENCODING      1212
-
-#define IDD_SETTINGS_BROWSER        110
-#define IDC_SET_DEFAULT_BROWSER     1301
-#define IDC_RESET_DEFAULT_BROWSER   1302
-
-#define IDD_SETTINGS_ABOUT          106
-#define IDC_UPDATES_LINK            1401
-#define IDC_HELP_LINK               1402
-#define IDC_SOURCE_LINK             1403
-#define IDC_AUTO_UPDATE             1404
-#define IDC_LEGAL_INFO              1405
-#define IDC_VERSION                 1406
 
 #define IDS_SETTINGS_CAPTION    200
 #define IDS_MENU_COMMAND        201
