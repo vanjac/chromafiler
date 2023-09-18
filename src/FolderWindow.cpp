e2fdd1e61bb06438f73459830f09df217f516263
j v <vantjac@gmail.com>
2023-09-17 20:35:04 -0700

    Forward IFolderFilter, fix hidden items reappearing after refresh

--- src/FolderWindow.cpp
+++ src/FolderWindow.cpp
@@ -38,8 +38,6 @@ const wchar_t * const HIDDEN_ITEM_PARSE_NAMES[] = {
     L"::{1CF1260C-4DD0-4EBB-811F-33C572699FDE}", // Music
     L"::{3ADD1653-EB32-4CB0-BBD7-DFA0ABB5ACCA}", // Pictures
     L"::{A0953C92-50DC-43BF-BE83-3742FED03C9C}", // Videos
-    // this appeared when IShellFolderView::SetCallback was added. compatibility behavior?
-    L"::{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}", // Control Panel "command object"
     // special items that are NOT hidden: user folder, This PC, Network, Recycle Bin, Linux
 };
 static CComPtr<IShellItem> controlPanelItem;
@@ -771,6 +769,8 @@ STDMETHODIMP FolderWindow::QueryInterface(REFIID id, void **obj) {
         QITABENT(FolderWindow, IWebBrowserApp),
         {},
     };
+    if (id == __uuidof(IFolderFilter) && prevCB)
+        return prevCB->QueryInterface(id, obj);
     HRESULT hr = QISearch(this, interfaces, id, obj);
     if (SUCCEEDED(hr))
         return hr;
@@ -908,6 +908,8 @@ STDMETHODIMP FolderWindow::OnViewCreated(IShellView *view) {
 
     return S_OK;
 }
+
+/* IShellFolderViewCB */
 
 STDMETHODIMP FolderWindow::MessageSFVCB(UINT msg, WPARAM wParam, LPARAM lParam) {
     if (msg == 17) { // refresh
